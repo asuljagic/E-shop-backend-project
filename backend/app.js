fba9e222e111018b87674327d8678f9984535caa
Adnan Suljagic <asuljagic1@etf.unsa.ba>
2022-03-31 13:44:48 +0200

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+require('dotenv/config');
+
+const api = process.env.API_URL;
+app.get('/', (req,res) => {
+    res.send('hello API!');
+})
+app.listen(3000, () => { 
+    console.log(api);
+    console.log('server is srunning http://localhost:3000');
+}) 
