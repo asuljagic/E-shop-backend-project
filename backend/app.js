eeeb67673d361b8a3c70e4fc0a7d685cea7b0fb7
Adnan Suljagic <asuljagic1@etf.unsa.ba>
2022-03-31 20:07:13 +0200

    refactoring and Product table model

--- backend/app.js
+++ backend/app.js
@@ -3,55 +3,37 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 
 require('dotenv/config');
+app.use(cors());
+app.options('*', cors());
+
 
 const api = process.env.API_URL;
+
+// routes
+const categoriesRoutes = require("./routes/categories");
+const productsRoutes = require("./routes/products");
+const usersRoutes = require("./routes/users");
+const ordersRoutes = require("./routes/orders");
+
 
 // middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-// product schema
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
-        required: true
-    }
-})
 
-// product model
-const Product = mongoose.model('Product', productSchema);
+// routers
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+const Product = require('./models/product');
 
 
-app.get(`${api}/products`, async (req,res) => {
-    const productList = await Product.find();
-
-    if(!productList){
-        res.status(500).json({success: false})
-    }
-    res.send(productList);
-})
-
-app.post(`${api}/products`, (req,res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-    
-    product.save().then((createdProduct => {
-        res.status(201).json(createdProduct);
-    })).catch((err) => {
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
-})
 
 mongoose.connect(process.env.CONNECTION_STRING)
 .then(() => {
