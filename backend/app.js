2a3d5eb5d7294b371f0f161f38f86f07bd4fb73a
Adnan Suljagic <asuljagic1@etf.unsa.ba>
2022-03-31 14:01:46 +0200

    added middleware

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,24 @@ const app = express();
 require('dotenv/config');
 
 const api = process.env.API_URL;
-app.get('/', (req,res) => {
-    res.send('hello API!');
-})
+
+// middleware
+app.use(express.json());
+
+app.get(`${api}/products`, (req,res) => {
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some_url',
+    }
+    res.send(product);
+});
+
+app.post(`${api}/products`, (req,res) => {
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
+});
 app.listen(3000, () => { 
-    console.log(api);
     console.log('server is srunning http://localhost:3000');
 }) 
